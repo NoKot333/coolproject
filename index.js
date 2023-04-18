99372ea856311ba11c4a619c882e12584982a3fc
Никита Опшин <nopshin-ki21@stud.sfu-kras.ru>
2023-04-18 19:33:53 +0700

    Hello, World1111

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-import express from 'express'
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('1111 Hello, World!')
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+});
