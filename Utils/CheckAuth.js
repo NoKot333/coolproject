a6c616860e731eca7faf2b5b304f6a1a70ede84d
Никита Опшин <nopshin-ki21@stud.sfu-kras.ru>
2023-04-24 19:28:45 +0700

    it have always worked

--- Utils/CheckAuth.js
+++ Utils/CheckAuth.js
@@ -1,13 +1,15 @@
     import jwt from 'jsonwebtoken';
 
     export default (req,res,next) => {
-        const token = (req.headers.authorization || '').replace(/Bearer\s?/,'');
+        //const token = (req.headers.authorization || '').replace(/Bearer\s?/,'');
+        const token = req.headers.authorization.split(' ')[1];
         if (token) {
             try {
                 const decoded = jwt.verify(token,'cipher');
                 req.userId = decoded._id;
                 next();
             } catch (err) {
+                console.log(token);
                 console.log(err);
                 return res.status(403).json({
                     message:'Нет доступа',
